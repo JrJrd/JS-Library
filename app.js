acc6108a8c39bbab3f3a9356a549ce62de7e638f
seaer <jrd.seaer@gmail.com>
2023-09-06 10:11:43 +0300

    DOM event w/ addbook logic

--- app.js
+++ app.js
@@ -16,12 +16,42 @@ class Book {
 class Library {
   constructor(books) {
     this.bookCount = books.length;
-    this.book = books;
+    this.books = books;
   }
 
-addBooks() {
+  addBook() {
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const read = document.getElementById("read");
+
+    const newBook = new Book(title.value, author.value, read.checked);
+    this.books.push(newBook);
+
+    this.bookCount++;
+    //select table body
+    const tbody = document.getElementById("tableBody");
+    //create a new table row
+    const newTr = document.createElement("tr");
+    //create new data cells
+    const newTitle = document.createElement("td");
+    const newAuthor = document.createElement("td");
+    const newRead = document.createElement("td");
+    // add text content to td's w/book values
+    newTitle.textContent = title.value;
+    newAuthor.textContent = author.value;
+    const newCheckbox = document.createElement("input");
+    newCheckbox.type = "checkbox";
+    newCheckbox.checked = read.checked;
+    newCheckbox.disabled = read.check;
     
-}
+    newRead.appendChild(newCheckbox);
+    //append td's to the tr
+    newTr.appendChild(newTitle);
+    newTr.appendChild(newAuthor);
+    newTr.appendChild(newRead);
+    //append tr to the body
+    tbody.appendChild(newTr);
+  }
 
   markRead(checkbox, id) {
     this.books.forEach((book) => {
@@ -33,3 +63,12 @@ addBooks() {
     });
   }
 }
+
+const library = new Library(books);
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  library.addBook();
+});
